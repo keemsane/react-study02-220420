2341f9258106c5bc53c4f0b8d05f40ab2d48ba44
keemsane <keemsane@gmail.com>
2022-04-21 09:35:06 +0900

    infojsx 문법에서 html 사용시 주의점.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.css';
-import Info01 from './info01';
+import Info from './info01';
 import Friends from './address';
 
-// 문제 1) 컴포넌트 componenet01.js 파일을 생성
+// 문제 1) 컴포넌트 info.01.js 파일을 생성
 //       → 자신의 간단한 정보를 출력하는 페이지 화면에 출력
 // 참고 1) src 폴더 아래에 components01.js 생성
 // 참고 2) 이름, 성별, 나이, 이메일, 주소 출력
@@ -21,7 +21,7 @@ function App() {
     <div>
       <h1>Profile</h1>
       <hr></hr>
-      <Info01 />
+      <Info />
       <hr></hr>
       <Friends
         name = "Cheolsoo"
--- src/address.js
+++ src/address.js
@@ -1,5 +1,10 @@
 import React from "react";
 
+// jsx 문법에서는 html 태그를 사용 시 반드시 시작태그와 끝태그가 존재해야함
+// js에서는 끝태그만 있거나 시작태그만 있어도 인식되지만
+// jsx는 xml이 합쳐진 형태이기 때문에 시작태그 끝태그를 모두 넣어줘야한다.
+// 참고. xml는 시작태그 끝태그 모두 필요로 한다.
+// <img> → <img></img>, <img/>
 function Friends(props) {
 
     // const name = props.name
