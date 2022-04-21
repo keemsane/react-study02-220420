fead5db3d2c6bcc5fe89d85a74b31d57cbac54de
keemsane <keemsane@gmail.com>
2022-04-21 10:41:45 +0900

    map 활용해서 배열 원소 (+ 이미지) 하나씩 출력

--- src/220421/App02.js
+++ src/220421/App02.js
@@ -12,16 +12,52 @@ import Food from './Food';
 //     );
 // }
 
+// 서버에서 전송된 데이터
+
+const foodLike = [
+    {
+        name : "KEEMCHY",
+        image : "http://img.choroc.com/newshop/goods/024444/024444_1.jpg",        
+    },
+    {
+        name : "LAMIEN",
+        image : "https://upload.wikimedia.org/wikipedia/commons/a/a9/%EB%8F%88%EC%BD%94%EC%B8%A0%EB%9D%BC%EB%A9%98.jpg",
+    },
+    {
+        name : "SAMGIOPSAL",
+        image : "https://cdn.mindgil.com/news/photo/202103/70839_7148_1250.jpg",
+    },
+    {
+        name : "CHUKKUMY",
+        image : "https://s3.ap-northeast-2.amazonaws.com/event-localnaeil/FileData/Article/202002/edfb543b-a18d-49f5-9aa7-12d2030da9ce.jpg",
+    },
+    {
+        name : "JOKBAL",
+        image : "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FWbMEr%2Fbtq329TgSg3%2FVXkAaCxbs1XbkHjDPQAWv1%2Fimg.png",
+    },
+    
+]   // foodLike 배열
+
+
 function App02() {
   return (
     <div>
       <h1>app.js를 app02.js로 대체해서 실습</h1>
       <hr />
       <h3>민철이가 좋아하는 랜덤 음식</h3>
-      <Food fav = "KIMCHI" />
-      <Food fav = "RAMEN" />
-      <Food fav = "SAMGYOPSAL" />
-      <Food fav = "CHUKUMEE" />
+      {/* <Food name = "KIMCHI" />
+      <Food name = "RAMEN" />
+      <Food name = "SAMGYOPSAL" />
+      <Food name = "CHUKUMEE" /> */}
+      
+      {/* Map 사용해서 배열 원소 하나씩 출력 ( + 화살표 함수 활용) */}
+      {foodLike.map((dish) => (
+        <Food 
+            name = {dish.name}
+            picture = {dish.image}
+            />
+      ))}
+      
     </div>
   );
 }
--- src/220421/Food.js
+++ src/220421/Food.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React from 'react';
+import './Food.css';
 
-function Food({fav}) {
-    return (
-        <h1>I LIKE {fav}</h1>
-
-    );
+function Food({ name, picture }) {
+  return (
+    <div>
+      <h2>I LIKE {name}</h2>
+      <img className="imgSize" src={picture} alt={name} />
+    </div>
+  );
 }
 
 export default Food;
