74cd3d3992ad0d8fe7d93a76736d329657822456
keemsane <keemsane@gmail.com>
2022-04-21 11:11:23 +0900

    props에 id(key) 추가

--- src/220421/App02.js
+++ src/220421/App02.js
@@ -16,22 +16,27 @@ import Food from './Food';
 
 const foodLike = [
     {
+        id : 1,
         name : "KEEMCHY",
         image : "http://img.choroc.com/newshop/goods/024444/024444_1.jpg",        
     },
     {
+        id : 2,
         name : "LAMIEN",
         image : "https://upload.wikimedia.org/wikipedia/commons/a/a9/%EB%8F%88%EC%BD%94%EC%B8%A0%EB%9D%BC%EB%A9%98.jpg",
     },
     {
+        id : 3,
         name : "SAMGIOPSAL",
         image : "https://cdn.mindgil.com/news/photo/202103/70839_7148_1250.jpg",
     },
     {
+        id : 4,
         name : "CHUKKUMY",
         image : "https://s3.ap-northeast-2.amazonaws.com/event-localnaeil/FileData/Article/202002/edfb543b-a18d-49f5-9aa7-12d2030da9ce.jpg",
     },
     {
+        id : 5, 
         name : "JOKBAL",
         image : "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FWbMEr%2Fbtq329TgSg3%2FVXkAaCxbs1XbkHjDPQAWv1%2Fimg.png",
     },
@@ -53,9 +58,10 @@ function App02() {
       {/* Map 사용해서 배열 원소 하나씩 출력 ( + 화살표 함수 활용) */}
       {foodLike.map((dish) => (
         <Food 
+            key = {dish.id}
             name = {dish.name}
             picture = {dish.image}
-            />
+            />            
       ))}
       
     </div>
--- src/220421/Food.js
+++ src/220421/Food.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import './Food.css';
+// 전달받은 데이터의 타입을 확인하기 위해서 prop-types 모듈을 로드
+import PropTypes from 'prop-types';
 
+/**
+ * 음식 데이터를 출력할 컴포넌트
+ * @param {*} 음식 이름
+ * @returns 내가 좋아하는 음식 이름
+ */
 function Food({ name, picture }) {
   return (
     <div>
@@ -11,3 +18,11 @@ function Food({ name, picture }) {
 }
 
 export default Food;
+
+// propTypes : 상위 컴포넌트에서 받은 데이터가 하위 컴포넌트에서 원하는 타입의 데이터인지 확인
+// isRequired : 필수 속성으로 설정, 해당 데이터가 없을 경우 오류 발생
+
+Food.propTypes = {
+    name : PropTypes.string.isRequired,
+    picture : PropTypes.string.isRequired,
+}
