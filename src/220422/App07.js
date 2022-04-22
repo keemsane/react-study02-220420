ea22ce33fd5942e55965cd34bb0aac704d784a5a
keemsane <keemsane@gmail.com>
2022-04-22 10:37:01 +0900

    async, await 사용 + 메서드로 axios 사용하기 + 콘솔 출력

--- src/220422/App07.js
+++ src/220422/App07.js
@@ -9,17 +9,21 @@ class App07 extends React.Component {
   };
 
   render() {
-
     const { isLoading } = this.state;
     // 변수 isLoading의 데이터 값에 따라 화면에 출력하는 문구를 변경
     return <div>{isLoading ? 'Loading....' : 'We are READY!!'}</div>;
   }
-  
+
+  getMovies = async () => {
+    const movies = await axios.get('https://yts.mx/api/v2/list_movies.json');
+    console.log(movies);
+  };
+
   componentDidMount() {
     // setTimeout(() => {
     //   this.setState({ isLoading: false });
     // }, 5000);
-    axios.get('https://yts.mx/api/v2/list_movies.json');
+    this.getMovies();
   }
 }
 
