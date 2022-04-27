c243226bb8f1de15d4bf5560aed6e4b0b5b67f1a
keemsane <keemsane@gmail.com>
2022-04-27 09:26:41 +0900

    HashRouter & RouterDom5버전으로 Detail 접근 성공

--- src/220422/App08.js
+++ src/220422/App08.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import { HashRouter, Link, Route, Routes } from 'react-router-dom';
-import { Routes, Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 import About from '../routes/About';
 import Home from '../routes/Home';
 import Detail from '../routes/Detail';
@@ -8,17 +8,15 @@ import Navigation from '../components/Navigation';
 
 function App08() {
   return (
-    <div>
+    <HashRouter>
       <Navigation />
-      <Routes>
-        <Route path="/" element={<Home />} exact={true} />
+        {/* <Route path="/" element={<Home />} exact={true} />
         <Route path="/about" element={<About />} />
-        <Route path="/movie-detail" element={<Detail />} />
-        {/* <Route path="/" component={Home} exact={true} />
+        <Route path="/movie-detail" element={<Detail />} /> */}
+        <Route path="/" component={Home} exact={true} />
         <Route path="/about" component={About} />
-        <Route path="/movie-detail" component={Detail} /> */}
-      </Routes>
-    </div>
+        <Route path="/movie-detail" component={Detail} />
+    </HashRouter>
   );
 }
 
